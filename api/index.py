14e69716a4474f1beb497c6762291e2a70f91290
Ratul Maharaj <ratulmaharaj@gmail.com>
2022-02-12 01:45:58 +0200

    added calc

--- api/index.py
+++ api/index.py
@@ -1,4 +1,7 @@
+from datetime import datetime
 from http.server import BaseHTTPRequestHandler
+import datetime as dt
+import numpy as np
 
 
 class handler(BaseHTTPRequestHandler):
@@ -6,7 +9,12 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
-        message = "Hello Alka"
+
+        days_off = 1
+        start = dt.date(2022, 2, 1)
+        end = dt.date.today()
+        days_since = np.busday_count(start, end) - days_off
+
+        message = f"{days_since}"
         self.wfile.write(message.encode())
         return
-
